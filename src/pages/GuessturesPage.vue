2e5bc5c51ef84c072543a60cc4c90b01893fc2cf
juanyaolin <maple96541@gmail.com>
2022-05-13 02:29:39 +0800

    feat(route): add guesstures and password game page and route

---
+++ src/pages/GuessturesPage.vue
@@ -0,0 +1,7 @@
+<template>
+  <q-page padding> </q-page>
+</template>
+
+<script>
+export default {};
+</script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,10 +1,18 @@
-
 const routes = [
   {
     path: "/",
     component: () => import("layouts/MainLayout.vue"),
     children: [
       { path: "", component: () => import("pages/IndexPage.vue") },
+      {
+        path: "guesstures",
+        component: () => import("pages/GuessturesPage.vue"),
+      },
+      {
+        path: "password",
+        component: () => import("pages/PasswordPage.vue"),
+      },
+    ],
   },
 
   // Always leave this as last one,
