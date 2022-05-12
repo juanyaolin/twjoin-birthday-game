1a04481c8bc1d1dd7644ea97fd13bebdc82d678a
juanyaolin <maple96541@gmail.com>
2022-05-13 02:43:28 +0800

    feat(page): add password game page

--- src/pages/PasswordPage.vue
+++ src/pages/PasswordPage.vue
@@ -1,7 +1,36 @@
 <template>
-  <q-page padding> </q-page>
+  <q-page padding>
+    <div class="q-gutter-md row">
+      <q-btn label="返回" to="/" />
+      <q-input outlined class="col" v-model="max" />
+      <q-btn label="取得" color="primary" @click="number = getNumber()" />
+    </div>
+
+    <div style="font-size: 10rem" class="text-center">
+      {{ number }}
+    </div>
+  </q-page>
 </template>
 
 <script>
-export default {};
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "PasswordPage",
+
+  setup() {
+    const max = ref(1000);
+    const number = ref(0);
+
+    function getNumber() {
+      return Math.floor(Math.random() * (this.max - 2)) + 2;
+    }
+
+    return {
+      max,
+      number,
+      getNumber,
+    };
+  },
+});
 </script>
